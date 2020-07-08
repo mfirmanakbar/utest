392c7b894635b0537b98d748dd7f18065a526df6
mfirmanakbar <em.firman.akbar@gmail.com>
2020-07-08 19:13:15 +0700

    mocking list multiple values

---
+++ src/test/java/com/firman/utest/business/ListMockTest.java
@@ -0,0 +1,35 @@
+package com.firman.utest.business;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDiffValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParams() {
+        when(mock.get(0)).thenReturn("firman");
+        assertEquals("firman", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+}
