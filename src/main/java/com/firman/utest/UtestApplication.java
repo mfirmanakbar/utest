c5b5fb6b2cf5d1edbee8bb3e95dbbd00d168b00b
M Firman Akbar <em.firman.akbar@gmail.com>
2020-07-08 11:39:20 +0700

    initial commit

---
+++ src/main/java/com/firman/utest/UtestApplication.java
@@ -0,0 +1,13 @@
+package com.firman.utest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firman/utest/UtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firman.utest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
